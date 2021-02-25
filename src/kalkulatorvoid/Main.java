fa3079e7db1cd28bc322585896049ce0c48463da
RizwandaaK <rizwanda_keysha_29rpl@student.smktelkom-mlg.sch.id>
2021-02-25 21:07:58 +0700

    KalkulatorVoid

---
+++ src/kalkulatorvoid/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalkulatorvoid;
+
+/**
+ *
+ * @author rizwa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        KalkulatorVoid ijiran = new KalkulatorVoid();
+        ijiran.KalkulatorVoid(500, 8);
+        ijiran.penjumlahan();
+        ijiran.pengurangan();
+        ijiran.perkalian();
+        ijiran.pembagian();
+    }
+    
+}
